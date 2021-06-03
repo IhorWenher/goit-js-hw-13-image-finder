ce29a8c2ffe1bb556aeaf443b85aeb26112488e8
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-06-03 10:24:38 +0300

    Робочий вариант

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import './sass/main.scss';
 import cardsTmpl from './image-markup.hbs';
 import NewApiService from './apiService';
 import { debounce } from 'debounce';
+import * as basicLightbox from 'basiclightbox';
 
 const refs = {
     input: document.querySelector('.js-input'),
     gallery: document.querySelector('.gallery'),
+    img: document.querySelector('img'),
     loadMoreBtn: document.querySelector('.load-more-btn'),
+    modalImageContainer: document.querySelector('.image-container'),
 };
 
 const newApiService = new NewApiService();
 
 refs.input.addEventListener('input', onSearchPhoto);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-
-function createImageMarkup(imageArray) {
-    return imageArray.map(cardsTmpl).join('');
-}
+refs.gallery.addEventListener('click', respondToTheTick);
 
 function onSearchPhoto(e) {
     refs.gallery.textContent = '';
@@ -28,7 +28,7 @@ function onSearchPhoto(e) {
 function onLoadMore() {
     newApiService.searchImage().then(appendImageMarkup);
 
-    refs.loadMoreBtn.scrollIntoView({
+    refs.gallery.lastElementChild.scrollIntoView({
         behavior: 'smooth',
         block: 'end',
     });
@@ -36,4 +36,17 @@ function onLoadMore() {
 
 function appendImageMarkup(image) {
     refs.gallery.insertAdjacentHTML('beforeend', createImageMarkup(image));
+}
+
+function createImageMarkup(imageArray) {
+    return imageArray.map(cardsTmpl).join('');
+}
+
+function respondToTheTick(e) {
+    const largeImageUrl = e.target.nextElementSibling.src;
+
+    const instance = basicLightbox.create(`<img src="${largeImageUrl}" width="800">`);
+    if (e.target.localName === 'img') {
+        instance.show();
+    }
 }
